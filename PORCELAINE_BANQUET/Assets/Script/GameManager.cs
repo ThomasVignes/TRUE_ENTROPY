5a3940d47f25e8143eefb4179206f43ef9f00eba
ThomasVignes <whumpus.wu@gmail.com>
2023-10-31 16:42:06 +0100

    Je sais pas

--- PORCELAINE_BANQUET/Assets/Script/GameManager.cs
+++ PORCELAINE_BANQUET/Assets/Script/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        player._Update();
+        player.Step();
 
     }
 }
--- PORCELAINE_BANQUET/Assets/Script/PlayerController.cs
+++ PORCELAINE_BANQUET/Assets/Script/PlayerController.cs
@@ -18,6 +18,10 @@ public class PlayerController : MonoBehaviour
     private bool rotating;
     private Vector3 targetPos;
     private Vector3 targetDir;
+    private Vector3 lastAgentVelocity;
+    private NavMeshPath lastAgentPath;
+
+    private bool computing;
 
     public bool Moving { get { return agent.remainingDistance > minDistanceToMove; } }
 
@@ -25,10 +29,24 @@ public class PlayerController : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
     }
-    public void _Update()
+    public void Step()
     {
         animator.SetBool("Walking", Moving && !rotating);
         
+        if (computing)
+        {
+            if (!agent.pathPending)
+            {
+                computing = false;
+
+                SetDirection();
+            }
+            else
+            {
+                return;
+            }
+        }
+
         if (Moving)
         {
             targetDir.y = transform.position.y;
@@ -43,20 +61,48 @@ public class PlayerController : MonoBehaviour
             else
             {
                 rotating = false;
-                agent.SetDestination(targetPos);
+                ResumePath();
             }
 
             Debug.Log(Vector3.Angle(transform.forward, targetDir));
         }
     }
 
+    private void SetDirection()
+    {
+        targetDir = Vector3.Normalize(agent.path.corners[1] - transform.position);
+
+        rotating = true;
+
+        PausePath();
+    }
+
     public void SetDestination(Vector3 pos)
     {
-        targetDir = Vector3.Normalize(pos - transform.position);
+        //targetDir = Vector3.Normalize(pos - transform.position);
 
         targetPos = pos;
 
-        rotating = true;
         agent.ResetPath();
+
+        agent.SetDestination(targetPos);
+
+        computing = true;
+    }
+
+    void PausePath()
+    {
+        lastAgentVelocity = agent.velocity;
+        lastAgentPath = agent.path;
+        agent.velocity = Vector3.zero;
+        agent.ResetPath();
+    }
+
+    void ResumePath()
+    {
+        agent.velocity = lastAgentVelocity;
+        agent.SetPath(lastAgentPath);
+
+        
     }
 }
