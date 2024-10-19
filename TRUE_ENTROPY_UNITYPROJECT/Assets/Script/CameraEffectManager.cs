9567bbe06b331c8d8575876b67b2075a8cc9fb6d
WhumpusWu <whumpus.wu@gmail.com>
2024-10-19 13:46:30 +0200

    Cinematic cool

--- TRUE_ENTROPY_UNITYPROJECT/Assets/Script/CameraEffectManager.cs
+++ TRUE_ENTROPY_UNITYPROJECT/Assets/Script/CameraEffectManager.cs
@@ -8,16 +8,40 @@ public enum CameraEffect
     None,
     Shake,
     Bump,
-    Rumble
+    Rumble,
+    Elevator
 }
 
 public class CameraEffectManager : MonoBehaviour
 {
-    [SerializeField] CinemachineImpulseSource shake, bump, rumble;
+    [SerializeField] CinemachineImpulseSource shake, bump, rumble, elevatorShake, elevatorBump;
+    [SerializeField] Shaking shaker;
     GameManager gm;
+
+    bool elevator;
+    float elevatorTimer;
+
     public void Init(GameManager man)
     {
         gm = man;
+    }
+
+    private void Update()
+    {
+        if (elevator)
+        {
+            if (elevatorTimer < Time.time)
+            {
+                elevatorBump.GenerateImpulse();
+                shaker.enabled = false;
+                elevator = false;
+            }
+            else
+            {
+                elevatorShake.GenerateImpulse();
+                elevatorShake.m_DefaultVelocity = new Vector3(elevatorShake.m_DefaultVelocity.x * -1,elevatorShake.m_DefaultVelocity.y * -1, elevatorShake.m_DefaultVelocity.z * -1);
+            }
+        }
     }
 
     public void PlayEffect(CameraEffect effect)
@@ -38,6 +62,20 @@ public class CameraEffectManager : MonoBehaviour
             case CameraEffect.Rumble:
                 rumble.GenerateImpulse();   
                 break;
+
+            case CameraEffect.Elevator:
+                StartCoroutine(C_Elevator());
+                break;
         }
+    }
+
+    IEnumerator C_Elevator()
+    {
+        elevatorBump.GenerateImpulse();
+
+        yield return new WaitForSeconds(2);
+
+        elevator = true;
+        elevatorTimer = Time.time + 10f;
     }
 }
