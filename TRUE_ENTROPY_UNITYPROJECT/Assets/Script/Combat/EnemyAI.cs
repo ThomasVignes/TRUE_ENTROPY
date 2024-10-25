57d391a56b0c4eca18ba5a25169432b6c218a65b
WhumpusWu <whumpus.wu@gmail.com>
2024-10-25 21:53:55 +0200

    pù

--- TRUE_ENTROPY_UNITYPROJECT/Assets/Script/Combat/EnemyAI.cs
+++ TRUE_ENTROPY_UNITYPROJECT/Assets/Script/Combat/EnemyAI.cs
@@ -10,18 +10,29 @@ public enum EnemyArchetype
     Scared
 }
 
+public enum EnemyFreeRoamArchetype
+{
+    Still,
+    Patrol,
+    Wander
+}
+
+
 public class EnemyAI : Character
 {
     [Header("AI Behaviour")]
     [SerializeField] EnemyArchetype archetype;
+    [SerializeField] EnemyFreeRoamArchetype freeRoamArchetype;
 
     [Header("AI Chase Settings")]
     [SerializeField] protected float attackDelay;
-    [SerializeField] protected float attackRange;
+    [SerializeField] protected float attackRange, windupRange;
+    [SerializeField] protected float attackRecovery;
 
-    [Header("AI Lose Settings")]
-    [SerializeField] protected float loseDelay;   
+    [Header("AI Search Settings")]
+    [SerializeField] protected float searchRange;
     [SerializeField] protected float loseRange;
+    [SerializeField] protected float loseDelay;
 
     [Header("Ai Floating Params")]
     [SerializeField] protected Character target;
@@ -30,10 +41,80 @@ public class EnemyAI : Character
     public override void Step()
     {
         base.Step();
+
+        if (target == null)
+        {
+            switch (freeRoamArchetype)
+            {
+                case EnemyFreeRoamArchetype.Still: 
+                    
+                    break;
+
+                case EnemyFreeRoamArchetype.Patrol: 
+                    
+                    break;
+                    
+                case EnemyFreeRoamArchetype.Wander: 
+                    
+                    break;
+            }
+
+            Detect();
+            return;
+        }
+
+        switch (archetype)
+        {
+            case EnemyArchetype.Neutral:
+                break; 
+            
+            case EnemyArchetype.Heavy:
+                HeavyLoop();
+                break;
+
+            case EnemyArchetype.Aggressor:
+                break;
+
+            case EnemyArchetype.Scared:
+                break;
+        }
     }
 
     public override void ConstantStep()
     {
         base.ConstantStep(); 
+    }
+
+    public void Detect()
+    {
+
+    }
+
+    public void HeavyLoop()
+    {
+        var dist = Vector3.Distance(transform.position, target.transform.position);
+
+        if (dist < attackRange)
+        {
+            if (dist < windupRange)
+            {
+                if (dist < attackRange)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+
+        }
     }
 }
