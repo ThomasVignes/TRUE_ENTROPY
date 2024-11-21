5d25d6832086a783c211d64250ebd917dfa1fba6
WhumpusWu <whumpus.wu@gmail.com>
2024-11-21 14:57:13 +0100

    Extra scene WIP

---
+++ TRUE_ENTROPY_UNITYPROJECT/Assets/LongestDayFirstManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LongestDayFirstManager : ChapterManagerGeneric
+{
+    public bool Skip;
+    [SerializeField] private string startCinematic;
+
+    public override void Init(GameManager gameManager)
+    {
+        base.Init(gameManager);
+    }
+
+    public override void IntroStep()
+    {
+
+    }
+
+    public override void StartGame()
+    {
+        if (Skip)
+        {
+            gameManager.Ready = true;
+            return;
+        }
+
+        StartCoroutine(C_Start());
+    }
+
+    IEnumerator C_Start()
+    {
+        gameManager.ScreenEffects.FadeTo(1, 0.01f);
+        AudioListener.volume = 0;
+
+        yield return new WaitForSeconds(2.3f);
+
+        AudioListener.volume = 1;
+
+        gameManager.CinematicManager.PlayCinematic(startCinematic);
+        gameManager.Ready = true;
+    }
+
+    public override void RestartGame()
+    {
+
+    }
+
+    public override void EndChapter()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public override void Death(string message)
+    {
+
+    }
+}
